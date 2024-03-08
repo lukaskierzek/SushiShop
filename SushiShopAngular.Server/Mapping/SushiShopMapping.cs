523765d3e330b6c8347a21f16b2b76e66eef6d85
Łukasz Kierzek <39835435+lukaskierzek@users.noreply.github.com>
2024-03-08 19:52:20 +0100

    Change name of SushiMapping to SushiShopMapping

--- SushiShopAngular.Server/Mapping/SushiShopMapping.cs
+++ SushiShopAngular.Server/Mapping/SushiShopMapping.cs
@@ -4,21 +4,25 @@ using SushiShopAngular.Server.Models.ModelsDTO;
 
 namespace SushiShopAngular.Server.Mapping
 {
-    public class SushiMapping : Profile
+    public class SushiShopMapping : Profile
     {
-        public SushiMapping()
+        public SushiShopMapping()
         {
             CreateMap<Sushi, SushiDTO>()
                 .ForMember(sushidto => sushidto.Description, c => c.MapFrom(sushi => sushi.Description.Description))
                 .ForMember(sushidto => sushidto.MainCategory, c => c.MapFrom(sushi => sushi.MainCategory.Name))
                 //.ForMember(sushidto => sushidto.Ingrediets, c => c.MapFrom(sushi => sushi.sushiIngredients.Select(y => y.Ingredient).ToList()))
                 .ForMember(sushidto => sushidto.Ingrediets, c => c.MapFrom(sushi => sushi.sushiIngredients))
-                .ForMember(sushidto => sushidto.SubCategories, c => c.MapFrom(sushi => sushi.sushiSubCategories.Select(y => y.SubCategory).ToList()));
+                .ForMember(sushidto => sushidto.SubCategories, c => c.MapFrom(sushi => sushi.sushiSubCategories.Select(ssc => ssc.SubCategory).ToList()));
 
             CreateMap<SubCategory, SubCategoryDTO>();
 
-            CreateMap<SushiIngredient, IngredientDTO>().IncludeMembers(src => src.Ingredient).ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Ingredient.Id));
-            CreateMap<Ingredient, IngredientDTO>().ForMember(idto => idto.Amount, c => c.Ignore());
+            CreateMap<SushiIngredient, IngredientDTO>()
+                .IncludeMembers(si => si.Ingredient)
+                .ForMember(ssc => ssc.Id, c => c.MapFrom(si => si.Ingredient.Id));
+            
+            CreateMap<Ingredient, IngredientDTO>()
+                .ForMember(idto => idto.Amount, c => c.Ignore());
 
             CreateMap<MainCategory, MainCategoryDTO>();
         }
